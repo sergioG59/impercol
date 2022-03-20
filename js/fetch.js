47ff81a66db0fee5e4dfca7e09e774043925937e
Your Name <you@example.com>
2022-03-20 13:09:00 -0500

    all new changes

---
+++ js/fetch.js
@@ -0,0 +1,41 @@
+const submitButon = document.querySelector("#submit");
+const form = document.querySelector("#form_control");
+const name = document.querySelector("#nombre");
+const Email = document.querySelector("#Email");
+const Asunto = document.querySelector("#Asunto");
+const message = document.querySelector("#mensaje");
+
+
+submitButon.addEventListener("click", onSubmit);
+
+function onSubmit(event) {
+    event.preventDefault();
+    console.log("Formulario de contacto IMPERCOL S.A ");
+    console.log("name:", nombre.value);
+    console.log("Email:", Email.value);
+    console.log("Asunto:", Asunto.value);
+    console.log("message:", mensaje.value);
+
+    const URL = "http://localhost:3000/form";
+
+    const body = {
+        nombre: nombre.value,
+        email: Email.value,
+        asunto: Asunto.value,
+        mensaje: mensaje.value
+    }
+
+    const parametros = {
+        headers: { "Content-Type": "text/plain; charset=UTF-8" },
+        body: JSON.stringify(body),
+        method: "POST"
+    }
+
+    fetch(URL, parametros)
+        .then(data => {
+            return data.json()
+        })
+        .then(res => console.log(res))
+        .then(error => console.error(error))
+
+}
